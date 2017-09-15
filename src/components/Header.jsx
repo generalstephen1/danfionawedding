567c752b5bb11e14a7dee51c56328ff8d6b0a49b
Stephen Utting <stephen.utting@thisisglobal.com>
2017-09-15 09:13:46 +0100

    mobile styles and build glitch

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,8 +16,8 @@ const Header = () => (
       </h1>
       <ul className="header__nav">
         <li className="header__nav-item"> <Link to="/Venue/">Venue</Link> </li>
-        <li className="header__nav-item"> <Link to="/Media/">Media</Link> </li>
         <li className="header__nav-item"> <Link to="/Contact/">Contact & RSVP</Link> </li>
+        <li className="header__nav-item"> <Link to="/Media/">Media</Link> </li>
       </ul>
     </div>
     <ConnectedCounter />
