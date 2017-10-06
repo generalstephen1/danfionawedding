3c30e72178bda5c51bc08e0bfac766eae33a3577
Stephen Utting <generalstephen1@gmail.com>
2017-10-06 11:36:36 +0100

    fix date.... oops

--- src/components/CountdownDisplay.jsx
+++ src/components/CountdownDisplay.jsx
@@ -7,7 +7,7 @@ const CountdownDisplay = (props) => {
     return (
 
         <div>
-            <h2 className="countdown__title">17th September 2018</h2>
+            <h2 className="countdown__title">16th August 2018</h2>
             <p className="countdown__text">{props.days} days, {props.hours}hrs {props.minutes}min {props.seconds}s to go!</p>
         </div>
     )
