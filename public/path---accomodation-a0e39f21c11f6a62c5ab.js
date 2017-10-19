546f2520bf9f64d2dc2532167fb4de62ef02ed1c
Stephen Utting <generalstephen1@gmail.com>
2017-10-19 10:11:43 +0100

    build 1.0.12 - correct deployment procedure

--- public/path---accomodation-a0e39f21c11f6a62c5ab.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0xaf5383d494a8],{"./node_modules/json-loader/index.js!./.cache/json/accomodation.json":function(o,n){o.exports={pathContext:{}}}});
-//# sourceMappingURL=path---accomodation-a0e39f21c11f6a62c5ab.js.map
--- public/path---media-a0e39f21c11f6a62c5ab.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0xe9f082cb06f8],{"./node_modules/json-loader/index.js!./.cache/json/media.json":function(e,o){e.exports={pathContext:{}}}});
-//# sourceMappingURL=path---media-a0e39f21c11f6a62c5ab.js.map
